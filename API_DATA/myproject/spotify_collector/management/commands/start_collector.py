8f2bc2986be55c2144dff47b2a5cc47d0bec36d3
Spencer Presley <spencerpresley96@gmail.com>
2024-03-03 03:01:03 -0500

    trying to swap branches

--- API_DATA/myproject/spotify_collector/management/commands/start_collector.py
+++ API_DATA/myproject/spotify_collector/management/commands/start_collector.py
@@ -1,10 +1,19 @@
 from django.core.management.base import BaseCommand
 import subprocess
+import webbrowser
+from django.conf import settings
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 class Command(BaseCommand):
     help = 'Starts the Spotify Data Collector Script'
     
     def handle(self, *args, **kwargs):
+        client_id = os.getenv('SPOTIPY_CLIENT_ID')
+        redirect_uri = os.getenv('SPOTIPY_REDIRECT_URI')
+        scopes 
         self.stdout.write(self.style.SUCCESS('Starting Spotify Data Collector...'))
         subprocess.run(['python3', '/Users/spencerpresley/HenHacks/Spotify_WrapUp_Plus/API_DATA/spotify_data_collector.py'])
         self.stdout.write(self.style.SUCCESS('Spotify data collector finished.'))
--- API_DATA/myproject/spotify_collector/views.py
+++ API_DATA/myproject/spotify_collector/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+import requests
+from dotenv import load_dotenv
+load_dotenv()
+import os
+
+def spotify_callback(request):
+    code = requests.GET.get('code')
+    if code:
+        token_url = 'https://accounts.spotify.com/api/token'
+        redirect_uri = os.getenv('SPOTIPY_REDIRECT_URI')
+        client_id = os.getenv('SPOTIPY_CLIENT_ID')
+        client_secret = os.getenv('SPOTIPY_CLIENT_SECRET')
+        
+        data = {
+            'grant_type': 'authorization_code',
+            'code': code,
+            'redirect_uri': redirect_uri,
+            'client_id': client_id,
+            'client_secret': client_secret,
+        }
+        
+        response = requests.post(token_url, data=data)
+        if response.status_code = 200:
+            access_token_info = response.json()
+            access_token = access_token_info['access_token']
+            return HttpResponse("Authetication success. You can close this window.")
+        else:
+            return HttpResponse("No code provided.")
+        
